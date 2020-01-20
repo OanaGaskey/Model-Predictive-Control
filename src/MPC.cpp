704148635b6e2e1b75195ae2ee13ad878e1a09cb
Oana <oana.gaskey@gmail.com>
2020-01-20 17:59:23 -0500

    set upper and lower limits for steering angle

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -183,7 +183,15 @@ std::vector<double> MPC::Solve(const VectorXd &state, const VectorXd &coeffs) {
     vars_lowerbound[i] = -1.0e19;
     vars_upperbound[i] = 1.0e19;
   }
-  
+
+  // The upper and lower limits of delta are set to -25 and 25
+  // degrees (values in radians).
+  // NOTE: Feel free to change this to something else.
+  for (int i = delta_start; i < a_start; i++) {
+    vars_lowerbound[i] = -1.0;
+    vars_upperbound[i] = 1.0;
+  }
+
   // Lower and upper limits for the constraints
   // Should be 0 besides initial state.
   Dvector constraints_lowerbound(n_constraints);
