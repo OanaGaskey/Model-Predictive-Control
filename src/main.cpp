8ce8abf741c5a600a1287df0812be6c6b20502ad
Oana <oana.gaskey@gmail.com>
2020-01-20 18:12:45 -0500

    added helper function to fit a polynomial

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,30 @@ double polyeval(Eigen::VectorXd coeffs, double x) {
   return result;
 }
 
+// Fit a polynomial.
+// Adapted from
+// https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
+Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
+                        int order) {
+  assert(xvals.size() == yvals.size());
+  assert(order >= 1 && order <= xvals.size() - 1);
+  Eigen::MatrixXd A(xvals.size(), order + 1);
+
+  for (int i = 0; i < xvals.size(); i++) {
+    A(i, 0) = 1.0;
+  }
+
+  for (int j = 0; j < xvals.size(); j++) {
+    for (int i = 0; i < order; i++) {
+      A(j, i + 1) = A(j, i) * xvals(j);
+    }
+  }
+
+  auto Q = A.householderQr();
+  auto result = Q.solve(yvals);
+  return result;
+}
+
 int main() {
   uWS::Hub h;
 
